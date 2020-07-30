4dd6d36b6bbd120884f6a81754641603674e47b3
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 20:00:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,20 @@ def load_library
 end
 
 def get_japanese_emoticon
-  # code goes here
+  result = "Sorry, that emoticon was not found"
+  data = load_library(file)
+  #data = {
+    # "happy" => {
+    #  :english => :) ,
+     # :japanese => )))
+   # }
+  #}
+  data.each do |emoti_name , emoti_trans|
+    if emoti_trans[:english] == western_emoticon
+      result = emoti_trans[:japanese]
+    end
+  end
+  result 
 end
 
 def get_english_meaning
