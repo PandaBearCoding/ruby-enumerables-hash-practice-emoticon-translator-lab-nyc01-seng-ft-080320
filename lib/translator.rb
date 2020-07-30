d889d40f889e94af9a214c90359ae54e9b754cf1
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 20:05:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,19 +4,16 @@ require 'yaml'
 require 'pry'
 
 def load_library
-  data = YAML.load_file(file)
-  result = {}
-  data.each do |emoti_name, emoti_trans|
-    english = emoti_trans[0]
-    japanese = emoti_trans[1]
-    result[emoti_name] = {
-        :english => english,
-        :japanese => japanese
-    }
-
-  end
-  result
-end
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  new_hash = {}
+  emoticons.each do |key, value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+    end
+  new_hash 
+end	end
 
 
 
